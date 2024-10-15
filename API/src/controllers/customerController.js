22a53865913776dc4187ab2e5762884f8be2b9f0
GordonGichini <gordongichini1@gmail.com>
2024-10-15 11:18:13 +0300

    controllers added

---
+++ API/src/controllers/customerController.js
@@ -0,0 +1,61 @@
+const Customer = require('../models/Customer');
+
+exports.createCustomer = async (req, res) => {
+    try {
+        const customer = await Customer.create(req.body);
+        res.status(200).json(customer);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getAllCustomers = async (req, res) => {
+    try {
+        const customers = await Customer.findAll();
+        res.status(200).json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getCustomerById = async (req, res) => {
+    try {
+        const customer = await Customer.findByPk(req.params.id);
+        if (customer) {
+            res.status(200).json(customer);
+        } else {
+            res.status(404).json({ message: 'Customer Not Found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.updateCustomer = async (req, res) => {
+    try {
+        const [updated] = await Customer.update(req.body, {
+            where: { id: req.params.id },
+        });
+        if (updated) {
+            const updatedCustomer = await Customer.findByPk(req.params.id);
+            res.status(200).json(updatedCustomer);
+        } else {
+            res.status(404).json({error: 'Customer Not Found'});
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.deleteCustomer = async (req, res) => {
+    try {
+        const deleted = await Customer.destroy({ where: { id: req.params.id }, });
+        if (deleted) {
+            res.status(200).send();
+        } else {
+            res.status(404).json({ message: 'Customer Not Found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
---
+++ API/src/controllers/projectController.js
@@ -0,0 +1,63 @@
+const Project = require('../models/project');
+
+exports.createProject = async (req, res) => {
+    try {
+        const project = await Project.create(req.body);
+        res.status(201).json(project);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.getAllProjects = async (req, res) => {
+    try {
+        const projects = await Project.findAll();
+        res.status(200).json(projects);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getProjectById = async (req, res) => {
+    try {
+        const project = await Project.findByPk(req.params.id);
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.updateProject = async (req, res) => {
+    try {
+        const [updated] = await Project.update(req.body, {
+            where: { id: req.params.id },
+        });
+        if (updated) {
+            const updatedProject = await Project.findByPk(req.params.id);
+            res.status(200).json(updatedProject);
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.deleteProject = async (req, res) => {
+    try {
+        const deleted = await Project.destroy({
+            where: { id: req.params.id },
+        });
+        if (deleted) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
